2dcb0c4fcf5b45a86cc37ae7d27c05585796d98b
seunghye00 <tmdgp1934@gmail.com>
2024-08-29 00:22:36 +0900

    프로젝트 생성
    프로젝트 생성 파일 공유

---
+++ HowsServer/src/main/java/com/hows/HowsServerApplication.java
@@ -0,0 +1,13 @@
+package com.hows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowsServerApplication.class, args);
+	}
+
+}
---
+++ HowsServer/src/test/java/com/hows/HowsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

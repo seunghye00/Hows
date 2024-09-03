c72fa270399ce5d21b3c4b916064c7058c3ef636
Sion-Noh <vmfpsel@gmail.com>
2024-09-03 18:39:47 +0900

    console 삭제

--- HowsFront/src/App.js
+++ HowsFront/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
 import {Home} from "./pages/Home/Home";
 import {Header} from "./components/Header/Header";
-import {host} from "./config/config";
 import {Footer} from "./components/Footer/Footer";
 
 function App() {
-
-  console.log("host ==== ", host);
 
   // 로딩
 

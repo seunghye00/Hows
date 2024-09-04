c152fcc9e99172b6a9a4e79f50ba78d1f623883c
Moontari <shaaa6256@gmail.com>
2024-09-04 16:56:01 +0900

    test123

--- HowsFront/src/components/Footer/Footer.jsx
+++ HowsFront/src/components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ export const Footer = () => {
                                         navicate('/mypage')
                                     }}
                                 >
-                                    고객센터1
+                                    고객센터
                                     <i class="bx bx-chevron-right"></i>
                                 </a>
                             </div>

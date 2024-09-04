9b1c4ffc9a9f864f51fc4c86a55e66fdaa22718c
Sion-Noh <vmfpsel@gmail.com>
2024-09-04 16:03:16 +0900

    git test

--- HowsServer/src/main/java/com/hows/cart/controller/CartController.java
+++ HowsServer/src/main/java/com/hows/cart/controller/CartController.java
@@ -24,7 +24,6 @@ public class CartController {
             return ResponseEntity.ok(list);
 
         } catch (Exception e){
-            e.printStackTrace();
             return null;
         }
     }

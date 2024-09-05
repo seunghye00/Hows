c07a19e37301a0e384e9f0753a30cbb14723966b
seunghye00 <tmdgp1934@gmail.com>
2024-09-05 11:27:50 +0900

    Revert "Merge branch 'dev' into feature/admin"
    This reverts commit 617c0a997b61b1d32490b150cc85727dc2a65d57, reversing
    changes made to dccb2db1a7f5b1c168f04c6939a5c7ab73ef6065.

--- HowsFront/src/pages/Home/Products/Main/Product/Item/Item.jsx
+++ HowsFront/src/pages/Home/Products/Main/Product/Item/Item.jsx
@@ -3,71 +3,7 @@ import styles from './Item.module.css'
 export const Item = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.contents}>
-          <div className={styles.item}>
-            <div className={styles.img}>img</div>
-            <div className={styles.title}>
-              <div>상품명</div>
-              <div>상품가격</div>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.img}>img</div>
-            <div className={styles.title}>
-              <div>상품명</div>
-              <div>상품가격</div>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.img}>img</div>
-            <div className={styles.title}>
-              <div>상품명</div>
-              <div>상품가격</div>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.img}>img</div>
-            <div className={styles.title}>
-              <div>상품명</div>
-              <div>상품가격</div>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.img}>img</div>
-            <div className={styles.title}>
-              <div>상품명</div>
-              <div>상품가격</div>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.img}>img</div>
-            <div className={styles.title}>
-              <div>상품명</div>
-              <div>상품가격</div>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.img}>img</div>
-            <div className={styles.title}>
-              <div>상품명</div>
-              <div>상품가격</div>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.img}>img</div>
-            <div className={styles.title}>
-              <div>상품명</div>
-              <div>상품가격</div>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.img}>img</div>
-            <div className={styles.title}>
-              <div>상품명</div>
-              <div>상품가격</div>
-            </div>
-          </div>
-        </div>
+        Item
     </div>
   );
 }

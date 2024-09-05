6fd855cce1cf4eca643112a1476d6f62e6b19024
yuna <yuna2101@naver.com>
2024-09-05 21:00:18 +0900

    마이페이지 집들이 ui구현
    마이페이지 집들이 ui구현

--- HowsFront/src/pages/Home/Mypage/Main/Scrap/Scrap.jsx
+++ HowsFront/src/pages/Home/Mypage/Main/Scrap/Scrap.jsx
@@ -5,6 +5,10 @@ export const Scrap = () => {
 
     return (
         <div className={styles.container}>
+            <div className={styles.countContents}>
+                <span>7</span>
+                <span>개의 스크랩</span>
+            </div>
             <div className={styles.contents}>
                 <div className={styles.feed}>
                     <img src={scrap}></img>

8183f96e951a2c8209f13e56eec26e563f24545a
seunghye00 <tmdgp1934@gmail.com>
2024-09-05 09:31:12 +0900

    관리자 페이지 영역 추가
    배너 관리 추가

--- HowsFront/src/pages/Admin/Admin.jsx
+++ HowsFront/src/pages/Admin/Admin.jsx
@@ -1,12 +1,14 @@
 import styles from './Admin.module.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Main } from './Main/Main'
+import { Banner } from './Banner/Banner'
 
 export const Admin = () => {
     return (
         <div className={styles.container}>
             <Routes>
                 <Route path="/" element={<Main />} />
+                <Route path="/banner" element={<Banner />}></Route>
             </Routes>
         </div>
     )
---
+++ HowsFront/src/pages/Admin/Banner/Banner.jsx
@@ -0,0 +1,5 @@
+import styles from './Banner.module.css'
+
+export const Banner = () => {
+    return <div className={styles.container}>배너</div>
+}

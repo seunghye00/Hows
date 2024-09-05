4fc4ce18de6cd75002cf58b0e692f24e2f274d1e
seunghye00 <tmdgp1934@gmail.com>
2024-09-05 16:11:53 +0900

    관리자 배너 관리 영역 DB 연동
    배너 목록 조회 기능 구현

--- HowsFront/src/pages/Admin/Banner/Banner.jsx
+++ HowsFront/src/pages/Admin/Banner/Banner.jsx
@@ -1,8 +1,23 @@
 import styles from './Banner.module.css'
 import { Button } from '../../../components/Button/Botton'
-import img from '../../../assets/images/banner01.png'
+import { bannerList } from '../../../api/banner'
+import { useEffect, useState } from 'react'
+import { formatDate } from '../../../commons/commons'
 
 export const Banner = () => {
+    const [banners, setBanners] = useState([])
+
+    useEffect(() => {
+        bannerList()
+            .then(resp => {
+                console.log(resp.data)
+                setBanners(resp.data) // 데이터 설정
+            })
+            .catch(error => {
+                console.log('데이터 가져오기 실패: ' + error) // 오류 처리
+            })
+    }, [])
+
     return (
         <>
             <div className={styles.btns}>
@@ -19,25 +34,23 @@ export const Banner = () => {
                     <div className={styles.cols}>등록 기간</div>
                 </div>
                 <div className={styles.list}>
-                    <div className={styles.rows}>
-                        <div className={styles.cols}>
-                            <input type="checkbox" name="" id="" />
+                    {banners.map((banner, i) => (
+                        <div key={i} className={styles.rows}>
+                            <div className={styles.cols}>
+                                <input type="checkbox" name="" id="" />
+                            </div>
+                            <div className={styles.cols}>
+                                {banner.banner_order}
+                            </div>
+                            <div className={styles.cols}>
+                                <img src="{banner.banner_url}" />
+                            </div>
+                            <div className={styles.cols}>
+                                {formatDate(banner.start_date)} ~{' '}
+                                {formatDate(banner.end_date)}
+                            </div>
                         </div>
-                        <div className={styles.cols}>1</div>
-                        <div className={styles.cols}>
-                            <img src={img} />
-                        </div>
-                        <div className={styles.cols}>24.09.05 ~ 24.09.10</div>
-                    </div>
-                    <div className={styles.rows}></div>
-                    <div className={styles.rows}></div>
-                    <div className={styles.rows}></div>
-                    <div className={styles.rows}></div>
-                    <div className={styles.rows}></div>
-                    <div className={styles.rows}></div>
-                    <div className={styles.rows}></div>
-                    <div className={styles.rows}></div>
-                    <div className={styles.rows}></div>
+                    ))}
                 </div>
             </div>
         </>
--- HowsServer/src/main/java/com/hows/banner/dto/BannerDTO.java
+++ HowsServer/src/main/java/com/hows/banner/dto/BannerDTO.java
@@ -1,5 +1,52 @@
 package com.hows.banner.dto;
 
-public class BannerDTO {
+import java.sql.Timestamp;
 
+public class BannerDTO {
+	private Long banner_seq;
+	private String banner_url;
+	private Timestamp start_date;
+	private Timestamp end_date;
+	private int banner_order;
+	public Long getBanner_seq() {
+		return banner_seq;
+	}
+	public void setBanner_seq(Long banner_seq) {
+		this.banner_seq = banner_seq;
+	}
+	public String getBanner_url() {
+		return banner_url;
+	}
+	public void setBanner_url(String banner_url) {
+		this.banner_url = banner_url;
+	}
+	public Timestamp getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(Timestamp start_date) {
+		this.start_date = start_date;
+	}
+	public Timestamp getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(Timestamp end_date) {
+		this.end_date = end_date;
+	}
+	public int getBanner_order() {
+		return banner_order;
+	}
+	public void setBanner_order(int banner_order) {
+		this.banner_order = banner_order;
+	}
+	public BannerDTO(Long banner_seq, String banner_url, Timestamp start_date, Timestamp end_date, int banner_order) {
+		super();
+		this.banner_seq = banner_seq;
+		this.banner_url = banner_url;
+		this.start_date = start_date;
+		this.end_date = end_date;
+		this.banner_order = banner_order;
+	}
+	public BannerDTO() {
+		super();
+	}
 }

046d97a1d765203614d0517e8196b2ac54c3382d
seunghye00 <tmdgp1934@gmail.com>
2024-09-06 09:07:24 +0900

    상품 페이지 SubCategory 컴포넌트 오류 수정
    import 오류 해결

--- HowsFront/src/pages/Home/Products/Main/Product/SubCategory/SubCategory.jsx
+++ HowsFront/src/pages/Home/Products/Main/Product/SubCategory/SubCategory.jsx
@@ -1,49 +1,51 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 import styles from './SubCategory.module.css'
-import axios from 'axios';
-import { host } from '../../../../../../config/config';
-import img01 from '../../../../../../assets/images/가구.png';
-import img02 from '../../../../../../assets/images/조명.png';
-import img03 from '../../../../../../assets/images/패브릭.png';
-import img04 from '../../../../../../assets/images/수납.png';
-import img05 from '../../../../../../assets/images/주방용품.png';
-import img06 from '../../../../../../assets/images/가전.png';
+import axios from 'axios'
+import { host } from '../../../../../../config/config'
+import img01 from '../../../../../../assets/images/가구.png'
+import img02 from '../../../../../../assets/images/조명.png'
+import img03 from '../../../../../../assets/images/패브릭.png'
+import img04 from '../../../../../../assets/images/수납.png'
+import img05 from '../../../../../../assets/images/주방용품.png'
+import img06 from '../../../../../../assets/images/가전.png'
 
 export const SubCategory = () => {
-    // 임시 이미지 
-    const images = [img01, img02, img03, img04, img05, img06];
+    // 임시 이미지
+    const images = [img01, img02, img03, img04, img05, img06]
 
     //카테고리 상태
-    const [categoriesList,setCategoriesList] = useState([]);
+    const [categoriesList, setCategoriesList] = useState([])
     useEffect(() => {
-        axios.get(`${host}/category`)
-        .then((resp) => {
-            // console.log(JSON.stringify(resp))
-            setCategoriesList(resp.data);  
-        })
-        .catch((err) => {
-            console.error(err); 
-        });
-    }, []);  // 컴포넌트가 처음 렌더링될 때 한 번만 실행
+        axios
+            .get(`${host}/category`)
+            .then(resp => {
+                // console.log(JSON.stringify(resp))
+                setCategoriesList(resp.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }, []) // 컴포넌트가 처음 렌더링될 때 한 번만 실행
 
-    
-  return (
-    <div className={styles.container}>
-        <div className={styles.title}>카테고리</div>
-        <div className={styles.item}>
-            {
-                categoriesList.map((item,i)=>{
-                    return(
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>카테고리</div>
+            <div className={styles.item}>
+                {categoriesList.map((item, i) => {
+                    return (
                         <div key={i} className={styles.categoryItem}>
                             <div>
-                                <img src={images[i]} alt={`category-${i}`} className={styles.image} />
+                                <img
+                                    src={images[i]}
+                                    alt={`category-${i}`}
+                                    className={styles.image}
+                                />
                             </div>
                             <div>{item.product_category_title}</div>
                         </div>
                     )
-                })
-            }
+                })}
+            </div>
         </div>
-    </div>
-  );
+    )
 }

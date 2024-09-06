caca76d78c05d12f996a1736b7231a1ea316d75c
Moontari <shaaa6256@gmail.com>
2024-09-06 09:41:57 +0900

    페이지 네이션 api 추가
    페이지 네이션  코드는 txt파일에 넣어둠
    라이브러리는 react-js-pagination
    사용

--- HowsFront/src/pages/Home/Community/Main/Main.jsx
+++ HowsFront/src/pages/Home/Community/Main/Main.jsx
@@ -1,11 +1,13 @@
 import styles from './Main.module.css'
 import { Sort } from './Sort/Sort'
 import { Category } from './Category/Category'
+import { Content } from './Content/Content'
 export const Main = () => {
     return (
         <div className={styles.communityWrap}>
             <Sort />
             <Category />
+            <Content />
         </div>
     )
 }

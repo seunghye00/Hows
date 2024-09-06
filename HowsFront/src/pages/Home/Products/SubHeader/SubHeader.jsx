a50cc1053413b47a87068b0bc2f9157c4667b73d
ol7mi <dmsal9865@gmail.com>
2024-09-06 17:04:18 +0900

    상품 무한스크롤/메뉴클릭효과/세부UI
    상품 무한스크롤/메뉴클릭효과/세부UI

--- HowsFront/src/pages/Home/Products/SubHeader/SubHeader.jsx
+++ HowsFront/src/pages/Home/Products/SubHeader/SubHeader.jsx
@@ -1,16 +1,44 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './SubHeader.module.css'
-import { Product } from '../Main/Product/Product';
+import { useState } from 'react';
 
 export const SubHeader = () => {
+
     const navi = useNavigate();
+
+    //로컬스토리지에서 activeMenu 값을 가져오거나 없으면 '쇼핑홈' 으로  초기화 
+    const [activeMenu, setActiveMenu] = useState(()=> localStorage.getItem('activeMenu' || '쇼핑홈'));
+
+    const handleMenuClick = menuName => {
+        setActiveMenu(menuName);
+
+        // 클릭할 때마다 로컬스토리지에 저장 
+        localStorage.setItem('activeMenu', menuName);
+
+        if (menuName === '쇼핑홈') {
+            navi('/products')
+        } else if (menuName === '카테고리') {
+            navi('/products/category')
+        } else if (menuName === '베스트') {
+            navi('/products/best')
+        }
+    }
 
     return (
         <div className={styles.container}>
             <ul className={styles.menu}>
-                <li onClick={()=>{navi('/products')}}>쇼핑홈</li>
-                <li onClick={()=>{navi('/products/category')}}>카테고리</li>
-                <li onClick={()=>{navi('/products/best')}}>베스트</li>
+                <li 
+                    className={`${styles.naviMenu} ${activeMenu === '쇼핑홈'? styles.active: ''}`}
+                    onClick={()=> handleMenuClick('쇼핑홈')}>쇼핑홈
+                </li>
+                <li 
+                    className={`${styles.naviMenu} ${activeMenu === '카테고리'? styles.active: ''}`}
+                    onClick={()=> handleMenuClick('카테고리')}>카테고리
+                </li>
+                <li 
+                    className={`${styles.naviMenu} ${activeMenu === '베스트'? styles.active: ''}`}
+                    onClick={()=> handleMenuClick('베스트')}>베스트
+                </li>
             </ul>
         </div>
     );

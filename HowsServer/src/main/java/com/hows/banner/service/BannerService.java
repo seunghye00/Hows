ec8f152c76ef235bc7d930fb8801e9c56b199fa1
seunghye00 <tmdgp1934@gmail.com>
2024-09-06 15:35:32 +0900

    파일 관련 코드 수정
    bucket 공통 변수 코드 적용

--- HowsServer/src/main/java/com/hows/banner/service/BannerService.java
+++ HowsServer/src/main/java/com/hows/banner/service/BannerService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -31,6 +32,9 @@ public class BannerService {
 	
 	@Autowired
 	private Storage storage;
+	
+    @Value("${gcp.bucket}")
+    private String bucket;
 
 	public List<BannerDTO> getAllBanners() {
 		List<Banner> list = bannRepo.findAll();
@@ -39,19 +43,18 @@ public class BannerService {
 
 	public boolean addBanner(MultipartFile file) {
 		try {
-			String bucketName = "sion-attachment";
 			String sysName = UUID.randomUUID().toString();
 			String contentType = file.getContentType();
 
 			// 업로드 하기 위한 정보 객체 생성
-			BlobId blobId = BlobId.of(bucketName, sysName);
+			BlobId blobId = BlobId.of(bucket, sysName);
 			BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(contentType).build();
 			// 실제 GCS에 업로드 하는 코드
 			Blob blob = storage.create(blobInfo, file.getBytes());
 			if (blob == null) {
 				return false;
 			}
-			String result = "https://storage.google.com/" + bucketName + "/" + sysName;
+			String result = "https://storage.google.com/" + bucket + "/" + sysName;
 			Banner banner = bannMap.toEntity(new BannerDTO(0L, sysName, result, null, null, 0));
 			bannRepo.save(banner);
 		} catch (Exception e) {
@@ -64,11 +67,9 @@ public class BannerService {
 	public boolean deleteBanner(String[] bannerNames) {
 		// 삭제 도중 실패 시 처리 방법 추후 수정
 		try {
-			String bucketName = "sion-attachment";
-			
 			for(String sysName : bannerNames) {
 				// 삭제하기 위한 정보 객체 생성
-				BlobId blobId = BlobId.of(bucketName, sysName);
+				BlobId blobId = BlobId.of(bucket, sysName);
 				// 파일 삭제
 		        boolean result1 = storage.delete(blobId);
 		        bannDao.deleteBySysName(sysName);

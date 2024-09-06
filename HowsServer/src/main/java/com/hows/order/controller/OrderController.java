bb01aa689bf0b34a9543223bc9299d6e673f43fa
seunghye00 <tmdgp1934@gmail.com>
2024-09-06 17:23:40 +0900

    관리자 주문 목록 관리 영역 서버 코드 작성
    주문 상태별 주문 목록 조회 기능 구현

--- HowsServer/src/main/java/com/hows/order/controller/OrderController.java
+++ HowsServer/src/main/java/com/hows/order/controller/OrderController.java
@@ -1,14 +1,18 @@
 package com.hows.order.controller;
 
-import com.hows.order.dto.OrderDTO;
-import com.hows.order.service.OrderService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.hows.order.dto.OrderDTO;
+import com.hows.order.dto.OrderInfoListDTO;
+import com.hows.order.service.OrderService;
 
 @RestController
 @RequestMapping("/order")
@@ -27,5 +31,18 @@ public class OrderController {
 			e.printStackTrace();
 			return null;
 		}
+	}
+
+	// 필터링된 주문 목록 조회
+	@GetMapping("/listByStatus")
+	public ResponseEntity<List<OrderInfoListDTO>> getOrdersByStatus(@RequestParam String status) throws Exception {
+		List<OrderInfoListDTO> list = orderServ.getOrdersByStatus(status);
+		return ResponseEntity.ok(list);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(Exception e) {
+		e.printStackTrace();
+		return "redirect:/error";
 	}
 }

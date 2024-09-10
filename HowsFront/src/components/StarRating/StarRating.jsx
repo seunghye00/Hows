94f8da19e37b4ef15033e857be55ec059127ba0f
ol7mi <dmsal9865@gmail.com>
2024-09-10 18:09:10 +0900

    상품 리뷰 별점 클릭 이벤트 추가

--- HowsFront/src/components/StarRating/StarRating.jsx
+++ HowsFront/src/components/StarRating/StarRating.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StarRatings from 'react-star-ratings';
 
-// (별점)
-const StarRating = ({ rating = 0, starDimension = "24px", starSpacing = "2px", numberOfStars = 5 }) => {
+const StarRating = ({ rating = 0, onRatingChange, starDimension = "24px", starSpacing = "2px", numberOfStars = 5 }) => {
     return (
         <StarRatings
             rating={rating}                        // 현재 별점 점수
@@ -10,6 +9,7 @@ const StarRating = ({ rating = 0, starDimension = "24px", starSpacing = "2px", n
             numberOfStars={numberOfStars}          // 별점 개수
             starDimension={starDimension}          // 별의 크기
             starSpacing={starSpacing}              // 별 사이 간격
+            changeRating={onRatingChange}          // 별점 변경 시 호출되는 함수
             name="rating"                          // 컴포넌트 이름
         />
     );

b38cc3db356f4481c4de0182e65a7dad1daf5a2d
yuna <yuna2101@naver.com>
2024-09-10 18:08:03 +0900

    modal css 수정 및 프로필 사진 변경 구현중
    - 마이페이지 조회할 내역 없으면 (  )이 없습니다. 표시
    - modal css 수정
    - 프로필 사진 변경 구현중

--- HowsFront/src/pages/Home/Mypage/UserDashboard/TextBox/TextBox.jsx
+++ HowsFront/src/pages/Home/Mypage/UserDashboard/TextBox/TextBox.jsx
@@ -1,11 +1,15 @@
 import styles from "./TextBox.module.css"
 
-export const TextBox = () => {
+export const TextBox = ({ text }) => {
 
     return (
 
         <div className={styles.container}>
+            <div className={styles.spanBox}>
+                <span className={styles.text}>{text}</span>
+                <span className={styles.text2}> 이 없습니다.</span>
 
+            </div>
         </div>
 
     )

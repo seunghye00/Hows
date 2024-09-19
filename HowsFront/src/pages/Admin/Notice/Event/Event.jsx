1853ae7d0b5a29dc6e64f33ae8fe3dbaf11da483
Minseo <wan7078@naver.com>
2024-09-19 19:20:19 +0900

    이벤트 페이지 페이징 추가
    페이징 만 추가 기능 x

--- HowsFront/src/pages/Admin/Notice/Event/Event.jsx
+++ HowsFront/src/pages/Admin/Notice/Event/Event.jsx
@@ -1,7 +1,14 @@
 import styles from './Event.module.css'
+import { Paging } from '../../../../components/Pagination/Paging'
 
 const Event = () => {
-    return <div className={styles.eventContainer}></div>
+    return (
+        <div className={styles.eventContainer}>
+            <div className={styles.pagination}>
+                <Paging />
+            </div>
+        </div>
+    )
 }
 
 export default Event
